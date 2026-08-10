07362b009071b153692993b93fa1bc3fc804fda4
aerial978 <mhathier@gmail.com>
2026-08-10 16:43:05 +0200

    security(database): disable PDO emulated prepares

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -69,6 +69,7 @@ class Database
             return new PDO($dsn, $user, $password, [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
                 PDO::MYSQL_ATTR_FOUND_ROWS   => true,
             ]);
         } catch (PDOException $e) {
