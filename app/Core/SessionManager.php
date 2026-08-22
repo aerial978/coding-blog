febd7f278f3358bd8797115d2a34077397d6f19b
aerial978 <mhathier@gmail.com>
2026-08-22 16:36:42 +0200

    fix: handle session name return type

--- app/Core/SessionManager.php
+++ app/Core/SessionManager.php
@@ -121,20 +121,23 @@ final class SessionManager implements SessionInterface
         }
 
         if (ini_get('session.use_cookies')) {
-            $params = session_get_cookie_params();
+            $params      = session_get_cookie_params();
+            $sessionName = session_name();
 
-            setcookie(
-                session_name(),
-                '',
-                [
-                    'expires' => time() - 42000,
-                    'path' => $params['path'],
-                    'domain' => $params['domain'],
-                    'secure' => $params['secure'],
-                    'httponly' => $params['httponly'],
-                    'samesite' => $params['samesite'],
-                ]
-            );
+            if ($sessionName !== false) {
+                setcookie(
+                    $sessionName,
+                    '',
+                    [
+                        'expires'  => time() - 42000,
+                        'path'     => $params['path'],
+                        'domain'   => $params['domain'],
+                        'secure'   => $params['secure'],
+                        'httponly' => $params['httponly'],
+                        'samesite' => $params['samesite'],
+                    ]
+                );
+            }
         }
 
         session_destroy();
